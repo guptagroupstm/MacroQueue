d1a568723528f1bd08cb875e7b28bbeb916b738f
Brad <imbrad@att.net>
2024-03-19 15:42:58 -0400

    Created __main__.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'MacroQueue',         # How you named your package folder (MyLib)
   packages = ['MacroQueue'],   # Chose the same as "name"
-  version = '0.1.5',      # Start with a small number and increase it with every change you make
+  version = '0.1.6',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Automating Scanning Probe Microscopy',   # Give a short description about your library
   author = 'Brad Goff',                   # Type in your name
